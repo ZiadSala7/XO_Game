733b13c1194955849d29c17a9182e212d970626e
Ziad Salah <zslah2800@gmail.com>
2024-08-03 21:06:51 +0300

    home view appRouter

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:go_router/go_router.dart';
+import 'package:xo_game/features/home/presentation/views/home_view.dart';
 import 'package:xo_game/features/login/presentation/view/login_view.dart';
 import 'package:xo_game/features/splash/presentation/view/splash_view.dart';
 
 class AppRouter {
   static const String loginView = "/loginView";
+  static const String homeView = "/homeView";
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -13,6 +15,10 @@ class AppRouter {
       GoRoute(
         path: AppRouter.loginView,
         builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: AppRouter.homeView,
+        builder: (context, state) => const HomeView(),
       ),
     ],
   );
