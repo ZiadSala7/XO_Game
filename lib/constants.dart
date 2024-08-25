97f761a9aa50e44fca16dbeddbe3e1a5dbc85777
ZiadSala7 <zslah2800@gmail.com>
2024-08-25 01:37:21 -0700

    const

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color scaffoldColor = Color.fromARGB(255, 32, 38, 58);
 const Color player1Color = Color.fromARGB(255, 170, 124, 124);
 const Color player2Color = Color.fromARGB(255, 102, 137, 103);
+const Color player3Color = Color.fromARGB(255, 102, 137, 103);
